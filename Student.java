36303c718cedc3253384047b92659f89c627cfc6
spnaaden <spnaaden@dmacc.edu>
2017-04-05 12:16:06 -0500

    Created search and delete method.

--- Student.java
+++ Student.java
@@ -2,12 +2,12 @@
 public class Student { //Referencing class Student
 	
 	//Creating private variables for each Student
-	private String firstName;
-	private String lastName;
-	private double GPA;
-	private boolean isDeansList; //3.50 GPA or Above
-	private boolean isPresidentsList; //4.00 GPA
-	private Student next;
+	public String firstName;
+	public String lastName;
+	public double GPA;
+	public boolean isDeansList; //3.50 GPA or Above
+	public boolean isPresidentsList; //4.00 GPA
+	public Student next;
 	
 	public Student (String firstname, String lastname, double gpa, Student nxt) { //Creating constructor for Students that includes a passed in first name, last name, and GPA value.
 		this.firstName = firstname;
--- StudentList.java
+++ StudentList.java
@@ -1,4 +1,71 @@
 
-public class StudentList {
+public class StudentList { //Referencing class StudentList.
 
+	//Creating private Student variables for StudentList operations.
+	private Student firstStudent;
+	private Student currentStudent;
+	private Student previousStudent;
+	IllegalArgumentException iae = new IllegalArgumentException();
+	
+	public StudentList() { //Creating constructor for StudentList.
+		firstStudent = null;
+	}
+	
+	public boolean isEmpty() { //Creating boolean method isEmpty to check if a StudentList is Empty.
+		return (firstStudent == null);
+	}
+	
+	public Student searchStudent(String studentLastName) { //Creating Student method searchStudent to search for a specific student in a StudentList (using a lastname from a student) returning the Student's position in memory if they exist.
+		 currentStudent = firstStudent;
+		while (studentLastName.compareTo(currentStudent.lastName) != 0) {
+			if (currentStudent.next == null) {
+				return null;
+			} else {
+				currentStudent = currentStudent.next;
+			}
+
+		}
+		return currentStudent;
+	}
+	
+	public Student deleteStudent(String studentLastName) { //Creating Student method deleteStudent to to search for a specific Student in a StudentList (using a lastname from a student), and then deleting the Student from the StudentList if they exist.
+		if (isEmpty() == true){
+			 throw iae;		
+		}
+		
+		currentStudent = firstStudent;
+		previousStudent = firstStudent;
+		while (currentStudent.lastName != studentLastName) {
+			if (currentStudent.next == null) {
+				return null;
+			} else {
+				previousStudent = currentStudent;
+				currentStudent = currentStudent.next;
+			}
+		}
+		if (currentStudent == firstStudent) {
+			firstStudent = firstStudent.next;
+		} else {
+			previousStudent.next = currentStudent.next;
+		}
+		return currentStudent;
+	}
+	
+	/*public void insertStudent(char keyData, char newData) { //Creating void method insertLink to search the LinkList for a specific link (using specified cData), and then inserting a new link right after that one with another cData value that was passed in.
+		if(isEmpty() == true) {
+			throw iae;		
+		}
+		
+		currentLink = first;
+		while (currentLink != null && !(currentLink.cData == (keyData))) {
+			currentLink = currentLink.next;
+		}
+		if (currentLink != null) {
+			currentLink.next = new Link(newData, currentLink.next);
+		}
+	} */
+	
+	
+	
+	
 }
